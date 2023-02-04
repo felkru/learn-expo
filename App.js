187109939e8835619cd490fdb0d5eb25fd1125cf
Felix Krückel <64913698+felkru@users.noreply.github.com>
2023-02-04 18:19:22 +0000

    added image and two buttons to app

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Wow! DAS war anstrengender als gedacht!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use default photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +25,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
   text: {
     color: "#fff",
